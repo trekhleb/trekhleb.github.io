398c08369d1ecbfd104fe2741fb770a0449cbaab
Oleksii Trekhleb <trehleb@gmail.com>
2021-01-09 09:17:23 +0100

    Do smth.

--- src/components/elements/PostPreview.tsx
+++ src/components/elements/PostPreview.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BlogPageQuery_allMarkdownRemark_nodes } from '../../pages/__generated__/BlogPageQuery';
+import Card from '../shared/Card';
+import CardContent from '../shared/CardContent';
+import H3 from '../shared/H3';
 
 type PostPreviewProps = {
   post: BlogPageQuery_allMarkdownRemark_nodes,
@@ -8,12 +11,18 @@ type PostPreviewProps = {
 const PostPreview = (props: PostPreviewProps): React.ReactElement | null => {
   const { post } = props;
 
+  const postTitle = (
+    <H3>
+      {post?.frontmatter?.title}
+    </H3>
+  );
+
   return (
-    <div>
-      <div>
-        {post?.frontmatter?.title}
-      </div>
-    </div>
+    <Card>
+      <CardContent>
+        {postTitle}
+      </CardContent>
+    </Card>
   );
 };
 
