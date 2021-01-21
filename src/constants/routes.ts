c7f570d15fe88b849769897b7ebd9844aac27874
Oleksii Trekhleb <trehleb@gmail.com>
2021-01-21 07:48:03 +0200

    Do smth.

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -10,17 +10,19 @@ export type Route = {
 
 type Routes = Record<Slug, Route>;
 
+// Add the "/" at the end of the path to make the classActive work properly
+// for navigation links.
 export const routes: Routes = {
   home: {
     path: '/',
     name: 'About',
   },
   projects: {
-    path: '/projects',
+    path: '/projects/',
     name: 'Projects',
   },
   blog: {
-    path: '/blog',
+    path: '/blog/',
     name: 'Blog',
   },
 };
