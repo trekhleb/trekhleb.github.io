4c86807bf74872a362aea6b81dc249fc6807a90a
Oleksii Trekhleb <trehleb@gmail.com>
2021-01-23 12:10:36 +0200

    Do smth.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ module.exports = {
           ...defaultTheme.fontFamily.sans,
         ],
       },
+      boxShadow: {
+        // The shadow-card class that is used for Cards.
+        // @see: https://tailwindcss.com/docs/box-shadow
+        card: '0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)',
+      },
     },
   },
   variants: {
