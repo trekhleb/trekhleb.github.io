c298d96bfacd9a9ca0beb5af5f3ca6675092ecf7
Oleksii Trekhleb <trehleb@gmail.com>
2021-01-26 10:47:56 +0200

    Do smth.

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,5 +1,5 @@
 // @see: https://tailwindcss.com/docs/text-color/
-export const activeLinkColor = 'text-pink-600';
+export const activeLinkColor = 'text-red-600';
 
 // @see: https://github.com/tailwindlabs/tailwindcss-typography#color-modifiers
 export const blogPostColor = 'prose-pink';
