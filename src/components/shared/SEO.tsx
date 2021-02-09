797483f091f1a40f0605566b7a19761cfceed3f0
Oleksii Trekhleb <trehleb@gmail.com>
2021-02-09 18:59:28 +0100

    Do smth.

--- src/components/shared/SEO.tsx
+++ src/components/shared/SEO.tsx
@@ -41,6 +41,8 @@ const SEO = (props: SEOProps): React.ReactElement => {
     ? `${windowNamePrefix} ${windowNameSeparator} %s`
     : `%s ${windowNameSeparator} ${windowNamePrefix}`;
 
+  const bannerURL = `${url}${image}`;
+
   return (
     <Helmet title={title} titleTemplate={titleTemplate}>
       <meta name="description" content={description} />
@@ -49,14 +51,14 @@ const SEO = (props: SEOProps): React.ReactElement => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={bannerURL} />
       {(isArticle ? true : null) && <meta property="og:type" content="article" />}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={bannerURL} />
     </Helmet>
   );
 };
