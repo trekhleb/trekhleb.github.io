c6aa074d5bbd3d741ce3d47210c62b72c58b91b2
Oleksii Trekhleb <trehleb@gmail.com>
2021-02-12 07:47:45 +0100

    Do smth.

---
+++ src/components/elements/ProjectFilters.tsx
@@ -0,0 +1,73 @@
+import React, { ChangeEvent } from 'react';
+import Row from '../shared/Row';
+
+export type SortOption = 'startDateAsc' | 'startDateDesc' | 'starsDesc';
+
+export const sortByStartDateAsc: SortOption = 'startDateAsc';
+export const sortByStartDateDesc: SortOption = 'startDateDesc';
+export const sortByStarsDesc: SortOption = 'starsDesc';
+
+type Sorter = {
+  title: string,
+  hidden?: boolean,
+}
+
+type Sorters = Record<SortOption, Sorter>;
+
+const sorters: Sorters = {
+  [sortByStartDateDesc]: {
+    title: '➘ start date',
+  },
+  [sortByStartDateAsc]: {
+    title: '➚ start date',
+    hidden: true,
+  },
+  [sortByStarsDesc]: {
+    title: '➘ stars',
+  },
+};
+
+type ProjectFiltersProps = {
+  sortBy: SortOption,
+  onSort: (sortKey: SortOption) => void,
+};
+
+const ProjectFilters = (props: ProjectFiltersProps): React.ReactElement => {
+  const { onSort, sortBy } = props;
+
+  const sorterOptions = Object.keys(sorters)
+    .filter((sorterKey: string) => {
+      const sorter = sorters[sorterKey as SortOption];
+      return !sorter?.hidden;
+    })
+    .map((sorterKey: string) => {
+      const sorter = sorters[sorterKey as SortOption];
+      return (
+        <option key={sorterKey} value={sorterKey}>
+          {sorter.title}
+        </option>
+      );
+    });
+
+  const onSortChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+    const selectedKey: SortOption = event.target.value as SortOption;
+    onSort(selectedKey);
+  };
+
+  return (
+    <div>
+      <Row>
+        <div className="text-sm text-gray-500 mr-1">
+          Sort by:
+        </div>
+        <div>
+          <select onChange={onSortChange} defaultValue={sortBy}>
+            {sorterOptions}
+          </select>
+        </div>
+      </Row>
+    </div>
+  );
+};
+
+export default ProjectFilters;
--- src/components/screens/ProjectsScreen.tsx
+++ src/components/screens/ProjectsScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Project as ProjectType } from '../../types/Project';
 import ProjectsList from '../elements/ProjectsList';
@@ -7,15 +7,67 @@ import PageHeader from '../shared/PageHeader';
 import Badge from '../shared/Badge';
 import Row from '../shared/Row';
 import SEO from '../shared/SEO';
+import ProjectFilters, {
+  sortByStarsDesc, sortByStartDateAsc,
+  sortByStartDateDesc,
+  SortOption,
+} from '../elements/ProjectFilters';
+import { getHitHubProjectStars } from '../../utils/project';
 
 type ProjectsScreenProps = {
   projects: ProjectType[],
 };
 
+type ProjectSorter = {
+  // Returns -1, 0, +1.
+  sort: (a: ProjectType, b: ProjectType) => number,
+};
+
+type ProjectSorters = Record<SortOption, ProjectSorter>;
+
+// @ts-ignore
+const projectSorters: ProjectSorters = {
+  [sortByStartDateDesc]: {
+    sort: (a: ProjectType, b: ProjectType): number => {
+      if (!a?.startDate || !b.startDate || a.startDate === b.startDate) {
+        return 0;
+      }
+      return a.startDate > b.startDate ? -1 : 1;
+    },
+  },
+  [sortByStartDateAsc]: {
+    sort: (a: ProjectType, b: ProjectType): number => {
+      if (!a?.startDate || !b.startDate || a.startDate === b.startDate) {
+        return 0;
+      }
+      return a.startDate < b.startDate ? -1 : 1;
+    },
+  },
+  [sortByStarsDesc]: {
+    sort: (a: ProjectType, b: ProjectType): number => {
+      const aStars = getHitHubProjectStars(a) || 0;
+      const bStars = getHitHubProjectStars(b) || 0;
+      if (aStars === bStars) {
+        return 0;
+      }
+      return aStars > bStars ? -1 : 1;
+    },
+  },
+};
+
 const ProjectsScreen = (props: ProjectsScreenProps): React.ReactElement => {
   const { projects } = props;
 
-  const projectsNum = projects.length;
+  const [sortBy, setSortBy] = useState<SortOption>(sortByStartDateDesc);
+  const [filteredProjects, setFilteredProjects] = useState<ProjectType[]>(projects);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    const sortedProjects = [...filteredProjects].sort(projectSorters[sortBy].sort);
+    setFilteredProjects(sortedProjects);
+  }, [sortBy]);
+
+  const projectsNum = filteredProjects.length;
 
   return (
     <PageLayout>
@@ -27,7 +79,10 @@ const ProjectsScreen = (props: ProjectsScreenProps): React.ReactElement => {
         <PageHeader>Projects</PageHeader>
         <Badge className="ml-3 self-start">{projectsNum}</Badge>
       </Row>
-      <ProjectsList projects={projects} />
+      <Row className="mb-6">
+        <ProjectFilters onSort={setSortBy} sortBy={sortBy} />
+      </Row>
+      <ProjectsList projects={filteredProjects} />
     </PageLayout>
   );
 };
