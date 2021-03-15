e1e36c725aa542003114b430108f6edf77aa6efb
Oleksii Trekhleb <trehleb@gmail.com>
2021-03-15 10:45:36 +0100

    Do smth.

--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -1,3 +1,12 @@
 import { SiteMetaData } from '../types/SiteMetaData';
+import { defaultSiteDescription, defaultSiteTitle, siteURL } from '../constants/siteMeta';
 
-export const siteMetadata: SiteMetaData = {};
+export const metaFieldTitle = 'title';
+export const metaFieldDescription = 'description';
+export const metaFieldSiteUrl = 'siteUrl';
+
+export const siteMetadata: SiteMetaData = {
+  [metaFieldTitle]: defaultSiteTitle,
+  [metaFieldDescription]: defaultSiteDescription,
+  [metaFieldSiteUrl]: siteURL,
+};
