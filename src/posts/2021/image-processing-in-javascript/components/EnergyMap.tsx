73352d5e68d494e5f6e954547ee8cb1328a20899
Oleksii Trekhleb <trehleb@gmail.com>
2021-03-25 07:31:21 +0100

    Do smth.

--- src/posts/2021/image-processing-in-javascript/components/EnergyMap.tsx
+++ src/posts/2021/image-processing-in-javascript/components/EnergyMap.tsx
@@ -1,10 +1,55 @@
 import React, { useRef, useEffect } from 'react';
-import { normalizeEnergyMap } from './imageResizerViz';
 import { setPixel } from './imageUtils';
 
 type EnergyMapProps = {
   energyMap: number[][] | null,
   className?: string,
+};
+
+const normalizeEnergy = (
+  energy: number,
+  maxEnergy: number,
+  maxNormalizedEnergy: number,
+): number => {
+  return Math.floor((energy / maxEnergy) * maxNormalizedEnergy);
+};
+
+const getMaxEnergy = (energyMap: number[][]): number => {
+  const height = energyMap.length;
+  const width = energyMap[0].length;
+
+  let maxEnergy = 0;
+  for (let y = 0; y < height; y += 1) {
+    for (let x = 0; x < width; x += 1) {
+      maxEnergy = Math.max(maxEnergy, energyMap[y][x]);
+    }
+  }
+
+  return maxEnergy;
+};
+
+const normalizeEnergyMap = (
+  energyMap: number[][],
+  maxNormalizedEnergy = 255,
+): number[][] => {
+  const height = energyMap.length;
+  const width = energyMap[0].length;
+
+  const maxEnergy = getMaxEnergy(energyMap);
+
+  const normalizedMap = new Array(height)
+    .fill(null)
+    .map(() => {
+      return new Array(width).fill(Infinity);
+    });
+
+  for (let y = 0; y < height; y += 1) {
+    for (let x = 0; x < width; x += 1) {
+      normalizedMap[y][x] = normalizeEnergy(energyMap[y][x], maxEnergy, maxNormalizedEnergy);
+    }
+  }
+
+  return normalizedMap;
 };
 
 const EnergyMap = (props: EnergyMapProps): React.ReactElement => {
