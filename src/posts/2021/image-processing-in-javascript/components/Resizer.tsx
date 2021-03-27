411334c952e47c804d84de479b5aed4a9af29b13
Oleksii Trekhleb <trehleb@gmail.com>
2021-03-27 09:18:40 +0100

    Do smth.

--- src/posts/2021/image-processing-in-javascript/components/Resizer.tsx
+++ src/posts/2021/image-processing-in-javascript/components/Resizer.tsx
@@ -13,8 +13,8 @@ import testImg from '../assets/test.jpg';
 const Resizer = (): React.ReactElement => {
   const [energyMap, setEnergyMap] = useState<EnergyMapType | null>(null);
 
-  const testImgRef = useRef<HTMLImageElement>(null);
-  const resultCanvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const onResizeIteration = (params: OnIterationParams): void => {
     const {
@@ -32,45 +32,43 @@ const Resizer = (): React.ReactElement => {
       seam,
       nrgMap,
     });
+
+    // ctx.putImageData(img, 0, 0);
   };
 
-  const resizeImage = (): void => {
-    const image: HTMLImageElement | null = testImgRef.current;
-    const resultCanvas: HTMLCanvasElement | null = resultCanvasRef.current;
-    if (!resultCanvas || !image) {
-      return;
-    }
-    const ctx: CanvasRenderingContext2D | null = resultCanvas.getContext('2d');
-    if (!ctx) {
-      return;
-    }
-
-    resultCanvas.width = image.width;
-    resultCanvas.height = image.height;
-
-    ctx.drawImage(image, 0, 0, resultCanvas.width, resultCanvas.height);
-    const imgData: ImageData = ctx.getImageData(0, 0, resultCanvas.width, resultCanvas.height);
-
-    resizeWidth(imgData, image.width - 1, onResizeIteration);
-
-    ctx.putImageData(imgData, 0, 0);
+  const resizeImage = (
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    img: HTMLImageElement,
+  ): void => {
+    const imgData: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    resizeWidth(imgData, img.width - 1, onResizeIteration);
+    // ctx.putImageData(imgData, 0, 0);
   };
 
   const resizeImageCallback = useCallback(resizeImage, []);
 
   useEffect(() => {
-    const image: HTMLImageElement | null = testImgRef.current;
-    if (!image) {
+    const img: HTMLImageElement | null = imgRef.current;
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    if (!canvas || !img) {
       return;
     }
-    image.addEventListener('load', resizeImageCallback);
+    const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+    if (!ctx) {
+      return;
+    }
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    img.addEventListener('load', () => resizeImageCallback(canvas, ctx, img));
   }, [resizeImageCallback]);
 
   return (
     <>
-      <img src={testImg} alt="Test source" ref={testImgRef} />
+      <img src={testImg} alt="Test source" ref={imgRef} />
       <EnergyMap energyMap={energyMap} className="mb-3" />
-      <canvas ref={resultCanvasRef} style={{ width: '100%' }} />
+      <canvas ref={canvasRef} style={{ width: '100%' }} />
     </>
   );
 };
