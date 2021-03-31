b5a4ff2986fd5367039c51ce28730854b18bdb23
Oleksii Trekhleb <trehleb@gmail.com>
2021-03-31 15:44:00 +0200

    Do smth.

--- src/posts/2021/image-processing-in-javascript/components/imageUtils.ts
+++ src/posts/2021/image-processing-in-javascript/components/imageUtils.ts
@@ -1,31 +1,17 @@
-export type Color = [r: number, g: number, b: number, a: number];
+export type Color = [r: number, g: number, b: number, a: number] | Uint8ClampedArray;
 
 export type Coordinate = [x: number, y: number];
 
-const xyToIndex = ([x, y]: Coordinate, width: number): number => {
-  return y * width + x;
+const xyToIndex = ([x, y]: Coordinate, imgWidth: number): number => {
+  return y * imgWidth + x;
 };
 
-const getPixelByIndex = (img: ImageData, index: number): Color => {
-  const i = index * 4;
-  const d = img.data;
-  return [d[i], d[i + 1], d[i + 2], d[i + 3]];
+export const getPixel = (imgData: ImageData, xy: Coordinate): Color => {
+  const index = xyToIndex(xy, imgData.width);
+  return imgData.data.subarray(index * 4, index * 4 + 4);
 };
 
-const setPixelByIndex = (img: ImageData, index: number, color: Color): void => {
-  const i = index * 4;
-  const d = img.data;
-  const [r, g, b, a] = color;
-  d[i] = r;
-  d[i + 1] = g;
-  d[i + 2] = b;
-  d[i + 3] = a;
-};
-
-export const getPixel = (img: ImageData, xy: Coordinate): Color => {
-  return getPixelByIndex(img, xyToIndex(xy, img.width));
-};
-
-export const setPixel = (img: ImageData, xy: Coordinate, color: Color): void => {
-  setPixelByIndex(img, xyToIndex(xy, img.width), color);
+export const setPixel = (imgData: ImageData, xy: Coordinate, color: Color): void => {
+  const index = xyToIndex(xy, imgData.width);
+  imgData.data.set(color, index * 4);
 };
