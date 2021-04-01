5752c18d5847ea3023675a40f770d0852daab4e7
Oleksii Trekhleb <trehleb@gmail.com>
2021-04-01 21:36:29 +0200

    Do smth.

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export type ButtonProps = {
+  children: React.ReactNode,
+  onClick?: () => void,
+  className?: string,
+  disabled?: boolean,
+  title?: string | undefined,
+  startEnhancer?: React.ReactNode,
+};
+
+const Button = (props: ButtonProps): React.ReactElement => {
+  const {
+    children,
+    className = '',
+    /* eslint-disable-next-line @typescript-eslint/no-empty-function */
+    onClick = (): void => {},
+    disabled = false,
+    title = undefined,
+    startEnhancer = null,
+  } = props;
+
+  const disabledClasses = disabled ? 'cursor-not-allowed bg-gray-500 hover:bg-gray-500 hover:text-white' : '';
+  const defaultClasses = 'hover:bg-white hover:text-black py-2 px-3 rounded shadow-sm border border-solid border-white hover:border-gray-300 bg-black text-white transition duration-200 ease-in-out flex flex-row items-center uppercase font-medium text-sm tracking-wider';
+  const classes = `${defaultClasses} ${disabledClasses} ${className}`;
+
+  const separator = startEnhancer ? (
+    <span className="w-2" />
+  ) : null;
+
+  return (
+    <button
+      className={classes}
+      onClick={onClick}
+      type="button"
+      disabled={disabled}
+      title={title}
+    >
+      {startEnhancer}
+      {separator}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
