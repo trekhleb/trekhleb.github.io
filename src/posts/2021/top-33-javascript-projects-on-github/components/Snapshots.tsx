edac81d5ce8ff2d4e42199f312fd45f62a6a5ad9
Oleksii Trekhleb <trehleb@gmail.com>
2021-11-12 17:48:28 +0100

    Do smth.

--- src/posts/2021/top-33-javascript-projects-on-github/components/Snapshots.tsx
+++ src/posts/2021/top-33-javascript-projects-on-github/components/Snapshots.tsx
@@ -9,9 +9,9 @@ import snapshot2020 from '../data/2020';
 const Snapshots = (): React.ReactElement | null => {
   const list = Object.values(snapshot2021).map((snapshot: SnapshotType, index: number) => {
     const kStarsHistory: (number | null)[] = [
-      snapshot2018[snapshot.id] ? snapshot2018[snapshot.id].kStars : null,
+      snapshot2018[snapshot.id] ? snapshot2018[snapshot.id].kStars : null, // 2018
       null, // 2019
-      snapshot2020[snapshot.id] ? snapshot2020[snapshot.id].kStars : null,
+      snapshot2020[snapshot.id] ? snapshot2020[snapshot.id].kStars : null, // 2020
       snapshot.kStars, // 2021
     ];
     return (
