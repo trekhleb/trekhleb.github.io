f200c6c977d69836b1095bed1f13e1d70826ff4a
trekhleb <trehleb@gmail.com>
2023-06-03 11:21:28 +0200

    Observe

--- src/posts/2023/observe/components/Section.tsx
+++ src/posts/2023/observe/components/Section.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { VscLink } from '@react-icons/all-files/vsc/VscLink';
+
 import { Divider } from './Divider';
 import { ContentType } from '../types/content';
 
@@ -6,18 +8,32 @@ type SectionProps = {
   type: ContentType,
   header: React.ReactNode,
   children: React.ReactNode,
+  hash: string,
 }
 
 export function Section(props: SectionProps): React.ReactElement {
-  const { children, type, header } = props;
+  const {
+    children,
+    type,
+    header,
+    hash,
+  } = props;
 
   return (
     <>
-      <div className="flex items-start">
-        <div className="flex bg-black text-xs text-white rounded-md px-2 py-1 mb-2 font-mono">{type}</div>
+      <div className="flex items-start mb-3" id={hash}>
+        <div className="flex bg-black text-xs text-white rounded-md px-2 py-1 font-mono">{type}</div>
       </div>
-      <div className="text-xl mb-8">{header}</div>
+
+      <div className="text-xl mb-8 group">
+        {header}
+        <a href={`#${hash}`} aria-label="Link" className="absolute text-black ml-3 mt-1 hidden group-hover:inline-block">
+          <VscLink className="text-black" />
+        </a>
+      </div>
+
       <div className="text-sm">{children}</div>
+
       <Divider />
     </>
   );
