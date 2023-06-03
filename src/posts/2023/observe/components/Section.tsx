eb30b41f3c380c3e1598cb24aea39b5a62148274
trekhleb <trehleb@gmail.com>
2023-06-03 17:10:52 +0200

    Observe

--- src/posts/2023/observe/components/Section.tsx
+++ src/posts/2023/observe/components/Section.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { VscLink } from '@react-icons/all-files/vsc/VscLink';
 
 import { Divider } from './Divider';
-import { ContentType } from '../types/content';
 
 type SectionProps = {
-  type: ContentType,
   header: React.ReactNode,
   children: React.ReactNode,
   hash: string,
@@ -14,20 +12,21 @@ type SectionProps = {
 export function Section(props: SectionProps): React.ReactElement {
   const {
     children,
-    type,
     header,
     hash,
   } = props;
 
+  const normalizedHash = hash.trim().toLowerCase().replace(/ /g, '-');
+
   return (
     <>
-      <div className="flex items-start mb-3" id={hash}>
-        <div className="flex bg-black text-xs text-white rounded-md px-2 py-1 font-mono">{type}</div>
+      <div className="flex items-start mb-3" id={normalizedHash}>
+        <div className="flex bg-black text-xs text-white rounded-md px-2 py-1 font-mono">{hash}</div>
       </div>
 
       <div className="text-xl mb-8 group">
         {header}
-        <a href={`#${hash}`} aria-label="Link" className="absolute text-black ml-3 mt-1 hidden group-hover:inline-block">
+        <a href={`#${normalizedHash}`} aria-label="Link" className="absolute text-black ml-3 mt-1 hidden group-hover:inline-block">
           <VscLink className="text-black" />
         </a>
       </div>
--- src/posts/2023/observe/types/content.ts
+++
@@ -1,5 +0,0 @@
-export enum ContentType {
-  Observation = 'observation',
-  Question = 'question',
-  Assumption = 'assumption',
-}
