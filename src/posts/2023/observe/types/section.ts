20388d0128a9b4a5dc608547cd1bf9879352346f
trekhleb <trehleb@gmail.com>
2023-06-05 11:07:40 +0200

    Observe

--- src/posts/2023/observe/types/section.ts
+++ src/posts/2023/observe/types/section.ts
@@ -1,4 +1,8 @@
 export enum SectionHash {
   Observation1 = 'observation 1',
+  Observation2 = 'observation 2',
+  Observation3 = 'observation 3',
+
   Assumption1 = 'assumption 1',
+  Assumption2 = 'assumption 2',
 }
