b235b6ba074282ade7216a0f35ecee416a8541cb
trekhleb <trehleb@gmail.com>
2023-06-10 21:15:45 +0200

    Observe

--- src/posts/2023/observe/components/Graph.tsx
+++ src/posts/2023/observe/components/Graph.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 
 import { SectionsContext } from '../contexts/sections';
 import { Spinner } from './Spinner';
+import { Section } from '../types/section';
 
 export function Graph(): React.ReactElement {
   const { sections } = React.useContext(SectionsContext);
 
   if (!sections || !Object.values(sections).length) return <Spinner />;
 
+  const sectionsArr: Section[] = Object.values(sections);
+
+  const sectionsCounter = (
+    <div className="text-sm">
+      Sections in total:{' '}
+      <span
+        className="text-xs rounded-md px-1 py-1 font-mono font-medium bg-black text-white"
+      >
+        {sectionsArr.length}
+      </span>
+    </div>
+  );
+
   return (
     <div>
-      Graph...
+      {sectionsCounter}
     </div>
   );
 }
