b78f06ad678901613a71407081f8a8f8ee9eb271
trekhleb <trehleb@gmail.com>
2023-06-16 15:04:13 +0200

    Observe

--- src/posts/2023/observe/data/graph.ts
+++ src/posts/2023/observe/data/graph.ts
@@ -1,5 +1,17 @@
 import { SectionNodePositions } from '../types/graph';
 
 export const nodePositions: SectionNodePositions = {
-  'assumption-wb3': { x: 132, y: 48 }, 'assumption-bz4': { x: 312, y: 48 }, 'observation-bs3': { x: 480, y: 192 }, 'observation-xgt': { x: 480, y: 264 }, 'observation-ht9': { x: 24, y: 108 }, 'observation-hx2': { x: 24, y: 192 }, 'assumption-ml1': { x: 228, y: 156 }, 'observation-vg2': { x: 480, y: 120 }, 'observation-me3': { x: 480, y: 48 }, 'observation-tg9': { x: 24, y: 264 }, 'assumption-cw5': { x: 192, y: 264 }, 'observation-pw8': { x: 96, y: 372 }, 'assumption-bq2': { x: 312, y: 336 },
+  'assumption-wb3': { x: 144, y: 48 },
+  'assumption-bz4': { x: 336, y: 48 },
+  'observation-bs3': { x: 504, y: 192 },
+  'observation-xgt': { x: 504, y: 264 },
+  'observation-ht9': { x: 24, y: 108 },
+  'observation-hx2': { x: 24, y: 192 },
+  'assumption-ml1': { x: 240, y: 156 },
+  'observation-vg2': { x: 504, y: 48 },
+  'observation-me3': { x: 504, y: 120 },
+  'observation-tg9': { x: 24, y: 264 },
+  'assumption-cw5': { x: 240, y: 264 },
+  'observation-pw8': { x: 72, y: 336 },
+  'assumption-bq2': { x: 312, y: 336 },
 };
