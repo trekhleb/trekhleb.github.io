bcc9e8feffc909ba30be39283f7a0cfa77cf9260
trekhleb <trehleb@gmail.com>
2023-08-07 09:19:04 +0200

    Add micrograd

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,7 @@ export const projects: Projects = {
     name: '🤖 Micrograd TS',
     srcURL: { url: 'https://github.com/trekhleb/micrograd-ts' },
     demoURL: { url: 'https://trekhleb.dev/micrograd-ts' },
-    cover: { srcPath: 'projects/micrograd-ts/01-cover.png' },
+    cover: { srcPath: 'projects/micrograd-ts/04-cover.png' },
     startDate: '2023-08-06',
     summary: [
       'A tiny scalar-valued autograd engine and a neural net on top of it. A TypeScript version of the karpathy/micrograd repo.',
