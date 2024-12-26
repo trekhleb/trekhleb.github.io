2a76de8da44c37b90539b15ae3e13eb3fcf9aad2
trekhleb <3000285+trekhleb@users.noreply.github.com>
2024-12-26 11:18:00 +0100

    Fix grammar

--- src/components/shared/Greeting.tsx
+++ src/components/shared/Greeting.tsx
@@ -14,26 +14,24 @@ const Greeting = (): React.ReactElement => {
 
   const projectsLinkElement = (
     <span className="inline-block">
-      <HyperLink link={projectsLink} className="underline font-medium underline-offset-2">projects</HyperLink>
+      <HyperLink link={projectsLink} className="underline underline-offset-2">projects</HyperLink>
     </span>
   );
 
   const blogLinkElement = (
     <span className="inline-block">
-      <HyperLink link={blogLink} className="underline font-medium underline-offset-2">articles</HyperLink>
+      <HyperLink link={blogLink} className="underline underline-offset-2">articles</HyperLink>
     </span>
   );
 
   return (
     <p className="font-light">
-      Hi there!
-      I&apos;m Oleksii. I work as a full-time software engineer. In my spare time I do
-      open-sourcing.
-      Most of my open-source {projectsLinkElement} are focused
-      on one thing &ndash; <i>to help people learn</i>. You may use these projects to
-      learn about algorithms in JavaScript and get prepared for technical interviews, or
-      to learn Python syntax and start experimenting with machine learning algorithms
-      and math behind them, etc. I also write {blogLinkElement} about life, web-development
+      Hi there! I&apos;m Oleksii, a full-stack software engineer. In my spare time, I do
+      open-sourcing. Most of my open-source {projectsLinkElement} focus on a single
+      goal &ndash; helping people learn. These projects can help you explore algorithms
+      in JavaScript and prepare for technical interviews, or learn Python syntax and
+      start experimenting with machine learning algorithms and the math behind them,
+      among other topics. I also write {blogLinkElement} about life, web development,
       and machine learning.
     </p>
   );
